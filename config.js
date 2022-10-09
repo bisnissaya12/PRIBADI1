5e19dff5ac573e119a063d852e4f6eff9a89d3a9
bisnissaya12 <112305650+bisnissaya12@users.noreply.github.com>
2022-10-09 17:41:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -276,7 +276,7 @@ global.APIs = { // API Prefix
 
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-''https://api.xteam.xyz': 'APIKEYMU',
+  'https://api.xteam.xyz': 'APIKEYMU',
   'https://anabotofc.herokuapp.com/': 'AnaBot',
   'https://api.lolhuman.xyz': 'Apikeymu',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
